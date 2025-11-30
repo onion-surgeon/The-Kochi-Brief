ceb63378fff55ee51a5bba2d393885dd88835f1f
onion-surgeon <mathewcrispin12@gmail.com>
2025-11-30 12:54:33 +0530

    type hinted columns for user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from app.core.db.base import Base
 
@@ -6,11 +8,36 @@ from app.core.db.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255),unique=True,nullable=False,index=True)
-    password_hash = Column(String(255),nullable=False)
-    username = Column(String(100),nullable=False)
-    is_active = Column(Boolean, default=True)
-    is_verified = Column(Boolean, default=False)
-    is_subscribed = Column(Boolean, default=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False, index=True
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String(255), nullable=False
+    )
+
+    username: Mapped[str] = mapped_column(
+        String(100), nullable=True
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, default=True
+    )
+
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False
+    )
+
+    is_subscribed: Mapped[bool] = mapped_column(
+        Boolean, default=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
